2d1f6f841882c78512dc2668642f932764f47d5b
Ziken <patrykkazmierczak97@gmail.com>
2017-07-17 21:20:43 +0200

    Calculator interface, deal with keyboard

---
+++ scripts/CalculatorInterface.js
@@ -0,0 +1,33 @@
+const CalculatorInterface = function (calcHandler) {
+    'use strict';
+    const operations        =    calcHandler.querySelector('.operations');
+    const calcInput         =    calcHandler.querySelector('.calc-input');
+    const arrowLeft         =    calcHandler.querySelector('.arrow-left');
+    const arrowRight        =    calcHandler.querySelector('.arrow-right');
+    let movingOperationArrows = true;
+
+    const SIGNS = { //operations signs
+        multi: '*', // multiplication
+        div: '/',   // division
+        add: '+',   // addition
+        sub: '-'    // subtraction
+    };
+    const memoryCont = calcHandler.querySelector('.memory-calc');
+    const memoryValues = new Set();
+    let whatCount = '';
+    let amountOperations = 0;
+    let isResult = false;
+
+    init = () => {
+        /*triggerButtons();
+        memoryInit();
+        keyboardInit();
+
+        arrowLeft.addEventListener('click',moveOperationsBlockLeft,false);
+        arrowRight.addEventListener('click',moveOperationsBlockRight,false);
+
+        calcHandler.querySelector('.calculations').addEventListener('click',function(){
+            calcInput.focus();
+        },false);
+    };*/
+};
---
+++ scripts/Keyboard.js
@@ -0,0 +1,92 @@
+/**
+ * Enable physical keyboard and this virtual one on the screen
+ * @param {Object} calcElement DOM element of calculator
+ * @param {Object} sendActionTo function which gets action of keyboard
+*/
+const Keyboard = function (calcElement, sendActionTo) {
+    'use strict';
+    const containerButtons  =    calcElement.querySelector('.buttons');
+    const buttons           =    Array.from(containerButtons.querySelectorAll('button'));
+
+    let init = () => {
+        triggerVirtualButtons();
+        triggerPhysicalButtons();
+    };
+    let triggerPhysicalButtons = () => {
+        calcElement.addEventListener('keydown',physicalKeyboardListener,false);
+    };
+    let triggerVirtualButtons = () => {
+        buttons.forEach( ( btn ) => {
+            btn.addEventListener('click', virtualKeyboardListener, false);
+            let img = btn.querySelector('img');
+            if ( img !== null ) {
+                img.removeEventListener('click', virtualKeyboardListener, false);
+            }//if
+        });
+    };
+    let physicalKeyboardListener = ( evt ) => {
+        let keyId = evt.keyCode || 0;
+        let action = '';
+        switch ( keyId ) {
+            case 8: //backspace
+                action = 'backspace';
+                break;
+            case 13: //enter
+                action = 'equality';
+                break;
+            case 27: //ESC
+                action = 'c';
+                break;
+            // numbers and nums
+            case 48:
+            case 49:
+            case 50:
+            case 51:
+            case 52:
+            case 53:
+            case 54:
+            case 55:
+            case 56:
+            case 57:
+            case 96:
+            case 97:
+            case 98:
+            case 99:
+            case 100:
+            case 101:
+            case 102:
+            case 103:
+            case 104:
+            case 105:
+                action = (keyId%48).toString();
+                break;
+            case 106:
+                action = 'multiply';
+                break;
+            case 107:
+                action = 'add';
+                break;
+            case 109:
+                action = 'substract';
+                break;
+            case 110:
+                action = 'dot';
+                break;
+            case 111:
+                action = 'divide';
+                break;
+            case 190:
+                action = 'dot';
+                break;
+            default:
+                action = '';
+        }
+        sendActionTo(action);
+    };
+    let virtualKeyboardListener = (evt) => {
+        let action = evt.target.dataset.val || '';
+        sendActionTo(action);
+    };
+
+    init();
+};
